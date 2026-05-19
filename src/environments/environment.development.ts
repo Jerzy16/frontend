8b8d71591b8577a548259e48db7bbdb0c5f0767d
jerzy <113630990+Jerzy16@users.noreply.github.com>
2026-05-19 03:38:53 -0500

    port cambio

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   //apiUrl: 'http://localhost:8081'
-  apiUrl: 'http://localhost:8081/api/productos'
+  apiUrl: 'http://localhost:8082/api/productos'
 
 };
